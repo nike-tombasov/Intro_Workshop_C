11523de42773a26a7a5024469cef961411ad7a2f
Nike Tombasov <nike.tombasov@ya.ru>
2022-10-18 17:17:42 +0300

    Created blocks for Final Work

--- FinalWork/Program.cs
+++ FinalWork/Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿string[] arrayIn = {"Some", "Things", "We", "Make", "!"};
+//Console.WriteLine("[{0}]", String.Join(" ", arrayIn));
+
+string[] arrayOut = new string[arrayIn.Length];
+Console.WriteLine("[{0}]", String.Join(" ", arrayOut));
+
