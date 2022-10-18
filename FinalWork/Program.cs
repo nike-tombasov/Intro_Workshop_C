e2e7d660d6870c1ddb3bd173457872b27efa22f4
Nike Tombasov <nike.tombasov@ya.ru>
2022-10-18 17:39:52 +0300

    Writed whole programm for Final Work

--- FinalWork/Program.cs
+++ FinalWork/Program.cs
@@ -1,6 +1,26 @@
-﻿string[] arrayIn = {"Some", "Things", "We", "Make", "!"};
-//Console.WriteLine("[{0}]", String.Join(" ", arrayIn));
+﻿string[] arrayIn = {"1.", "Some", "Things", "We", "Make", "!", ":-)"};
+string[] arrayTemp = new string[arrayIn.Length];
+int j = 0;
+Console.WriteLine("Input array:");
+Console.WriteLine("[{0}]", String.Join(" ", arrayIn));
+Console.WriteLine();
 
-string[] arrayOut = new string[arrayIn.Length];
+for (int i = 0; i < arrayIn.Length; i++)
+{
+    if (arrayIn[i].Length <= 3)
+    {
+        arrayTemp[j] = arrayIn[i];
+        j++;
+    }
+}
+
+string[] arrayOut = new string[j+1];
+
+for (int k = 0; k < j; k++)
+{
+    arrayOut[k] = arrayTemp[k] ;
+}
+
+Console.WriteLine("Filtered array with strings that have 3 symbols or less:");
 Console.WriteLine("[{0}]", String.Join(" ", arrayOut));
 
